2fde2f1614b5d066c7161e55c83793b4fc0b20c9
Justas Dapkūnas <justas.dapkunas@gmail.com>
2021-01-25 15:41:13 +0200

    Fix redirects for production server
    	modified:   comer.conf
    	modified:   comer_web/settings/__init__.py

--- comer_web/settings/__init__.py
+++ comer_web/settings/__init__.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 STATIC_ROOT = '/opt/comer_web/static'
 
