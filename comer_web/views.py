f942a57f5e60c79671b5b213db787e6aff4936c0
Justas Dapkūnas <justas.dapkunas@gmail.com>
2021-11-10 00:13:47 +0200

    Add small UI fixes
    	modified:   comer_web/views.py
    	modified:   templates/base.html
    	modified:   templates/jobs/not_finished_or_removed.html
    	modified:   templates/search/results.html
    	modified:   templates/site/api_help.html

--- comer_web/views.py
+++ comer_web/views.py
@@ -21,7 +21,13 @@ def api_help(request):
     available_databases['COTHER'] = nice_db_names(settings.COTHER_DATABASES)
     available_databases['HHsuite'] = nice_db_names(settings.HHSUITE_DATABASES)
     available_databases['HMMER'] = nice_db_names(settings.SEQUENCE_DATABASES)
+    short_db_names = {}
+    short_db_names['COMER'] = [d[0] for d in settings.COMER_DATABASES]
+    short_db_names['COTHER'] = [d[0] for d in settings.COTHER_DATABASES]
+    short_db_names['HHsuite'] = [d[0] for d in settings.HHSUITE_DATABASES]
+    short_db_names['HMMSR'] = [d[0] for d in settings.SEQUENCE_DATABASES]
     context['db'] = available_databases
+    context['short_db_names'] = short_db_names
     return render(request, 'site/api_help.html', context)
 
 def availability(request):
