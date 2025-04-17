1bf4c424415fa339be178be1e445511fd3e583fe
Justas Dapkūnas <justas.dapkunas@gmail.com>
2025-04-17 18:21:36 +0300

    Load PDB annotations to database
    	new file:   apps/databases/management/commands/load_PDB_data.py
    	modified:   apps/databases/models.py

---
+++ apps/databases/management/commands/load_PDB_data.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from apps.databases import models as databases_models
+
+
+class Command(BaseCommand):
+    help = 'Load PDB data from a locally downloaded PDB SEQRES file'
+    # PDB SEQRES file url: 
+    # https://files.wwpdb.org/pub/pdb/derived_data/pdb_seqres.txt
+
+    def add_arguments(self, parser):
+        parser.add_argument('pdb_seqres_file', help="PDB SEQRES file")
+
+    def handle(self, *args, **options):
+        with open(options['pdb_seqres_file']) as f:
+            for line in f:
+                if line.startswith('>'):
+                    data, annotation = line[1:].rstrip().split('  ', 1)
+                    pdb_chain, mol_type, other = data.split(' ', 2)
+                    if mol_type != 'mol:protein':
+                        continue
+                    pdb_id, chain = pdb_chain.split('_', 1)
+                    print(
+                        'Inserting annotation for %s (%s)' % (pdb_chain,
+                                                              annotation
+                                                              )
+                        )
+                    pdb_obj, created = databases_models.PDB\
+                        .objects.update_or_create(id=pdb_id)
+                    annotation_obj, created = databases_models.PDBAnnotation\
+                        .objects.update_or_create(annotation=annotation)
+                    databases_models.Chain.objects.update_or_create(
+                        pdb=pdb_obj, chain=chain, annotation=annotation_obj
+                        )
+
--- apps/databases/models.py
+++ apps/databases/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# UniProt data
 class UniProt(models.Model):
     uniprot_ac = models.CharField(
         max_length=20, unique=True, db_collation='utf8_bin'
@@ -8,6 +9,32 @@ class UniProt(models.Model):
     annotation = models.TextField(db_collation='utf8_bin')
 
 
+# PDB data tables
+class PDB(models.Model):
+    id = models.CharField(
+        max_length=20, unique=True, db_collation='utf8_bin', primary_key=True
+        )
+
+
+class PDBAnnotation(models.Model):
+    annotation = models.TextField(db_collation='utf8_bin', unique=True)
+
+
+class Chain(models.Model):
+    pdb = models.ForeignKey(PDB, on_delete=models.CASCADE)
+    chain = models.CharField(max_length=20, db_collation='utf8_bin')
+    annotation = models.ForeignKey(PDBAnnotation, on_delete=models.RESTRICT)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['pdb', 'chain'],
+                name='unique_pdb_chain'
+                ),
+            ]
+
+
+#ECOD data
 class ECOD(models.Model):
     uid = models.IntegerField(unique=True)
     ecod_domain_id = models.CharField(
